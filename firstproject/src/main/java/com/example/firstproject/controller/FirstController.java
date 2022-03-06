2f81116f649056a4d375132502dfc6fe17aa00bd
metacogpe <kakao.com>
2022-03-06 17:10:55 +0900

    FirstController

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,14 @@
+// greetings.mustache 뷰페이지를 볼 수 있게 연결시키는 Controller 코드
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;  //컨트롤러 선언 어노테이션하면 자동으로 임포트 됨
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // 컨트롤러임을 선언
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou() {
+        return "greetings"; // templates/greetings.mustache 를 브라우저로 전송하게 됨
+    }
+}
