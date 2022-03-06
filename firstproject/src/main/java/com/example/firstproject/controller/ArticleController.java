b9b16a5ec19d5689f8a3d03c6ebfb12be21169f7
metacogpe <kakao.com>
2022-03-06 22:03:44 +0900

    articleRepository.save(article)

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,6 +2,8 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,10 @@ import javax.swing.text.html.parser.Entity;
 
 @Controller
 public class ArticleController {
+
+    // 객체 주입하기 DI
+    @Autowired  // 스프링부트가 미리 생성해 놓은 객체를 가져다가 자동 연결!
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -21,7 +27,11 @@ public class ArticleController {
         System.out.println(form.toString());
         // 1. DTO를 변환 :Entity
         Article article = form.toEntity();
-        // 2. Repository가 Entity를 DB에 저장함 
+        System.out.println(article.toString());
+        // 2. Repository가 Entity를 DB에 저장함
+        Article saved = articleRepository.save(article);  //위에서 Entity로 만들어진 article를 save
+        System.out.println(saved.toString());
+
         return "";
     }
 }
