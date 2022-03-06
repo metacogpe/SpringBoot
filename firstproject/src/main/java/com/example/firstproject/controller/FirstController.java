b24da0592ec5ea54fb048124731867db317f74ab
metacogpe <kakao.com>
2022-03-06 19:23:31 +0900

    template model

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -2,13 +2,15 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;  //컨트롤러 선언 어노테이션하면 자동으로 임포트 됨
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // 컨트롤러임을 선언
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "홍길동");  // 모델의 속성
         return "greetings"; // templates/greetings.mustache 를 브라우저로 전송하게 됨
     }
 }
