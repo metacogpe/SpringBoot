231ecf67da4b62bba9b71c0116f6ad7fb16c9391
metacogpe <kakao.com>
2022-03-06 21:11:56 +0900

    GetMapping:article/new->/article/create->dto:ArticleForm->PostMapping

---
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,22 @@
+// dto : 데이터 받을 그릇
+package com.example.firstproject.dto;
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // title와 content을 확인하기 위한 toString
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
