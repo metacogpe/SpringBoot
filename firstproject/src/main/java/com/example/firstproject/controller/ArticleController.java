71ccb9940d05f7660f59fffd5551ee5e333f4509
metacogpe <kakao.com>
2022-03-08 23:05:42 +0900

    refactoring with lombok : logging

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.swing.text.html.parser.Entity;
 
 @Controller
+@Slf4j  // 로깅을 위한 어노테이션
 public class ArticleController {
 
     // 객체 주입하기 DI
@@ -24,13 +26,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create")   //createArticle()의 파라미터에 dto의 ArticleForm을 입력
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());  // 아래의 println 대체
+//      System.out.println(form.toString()); --> 로깅 기능으로 대체
         // 1. DTO를 변환 :Entity
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+//        System.out.println(article.toString());
         // 2. Repository가 Entity를 DB에 저장함
         Article saved = articleRepository.save(article);  //위에서 Entity로 만들어진 article를 save
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
 
         return "";
     }
